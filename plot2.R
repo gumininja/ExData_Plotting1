ab064f4ca421409413ef0f4f31435d44b4aabe9a
Attila Mravik <evik.gm@gmail.com>
2017-05-14 11:54:50 +0200

    fix naming

--- plot2.R
+++ plot2.R
@@ -3,6 +3,6 @@ plot2 <- function() {
     power_target_dates <- power[grep("^[12]/2/2007", power$Date), ]
     global_active_power <- power_target_dates[,"Global_active_power"]
     converted <- as.numeric(as.character(global_active_power))
-    dateTime <- strptime(paste(power_target_dates$Date, power_target_dates$Time), "%d/%m/%Y %H:%M:%S")
-    plot(dateTime, converted, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+    datetime <- strptime(paste(power_target_dates$Date, power_target_dates$Time), "%d/%m/%Y %H:%M:%S")
+    plot(datetime, converted, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 }
